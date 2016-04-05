b2e90a2f14a37c76fe939017bd57f7a07bf21fcf
shiqiyue <835802084@qq.com>
2016-04-05 10:37:41 +0800

    test

---
+++ mongo_test/test/mongo_test/TestAdd.java
@@ -0,0 +1,14 @@
+package mongo_test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestAdd {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
