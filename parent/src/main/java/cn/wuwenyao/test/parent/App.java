28d1db460abe824330f944dac038d4839f9a6b7c
shiqiyue <835802084@qq.com>
2016-04-05 10:10:46 +0800

    init

---
+++ parent/src/main/java/cn/wuwenyao/test/parent/App.java
@@ -0,0 +1,13 @@
+package cn.wuwenyao.test.parent;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
